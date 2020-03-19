6cc5154910c0fa82e3c8a71fdf608c1d5f549ac5
Satyam Sharma <satyamsharma251104@gmail.com>
2020-03-19 13:58:58 +0400

    What is your name ?

--- Scanner/src/com/company/Main.java
+++ Scanner/src/com/company/Main.java
@@ -5,14 +5,10 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        System.out.println("What is your name?");
         Scanner in = new Scanner(System.in);
         String s = in.nextLine();
-        System.out.println("You entered string "+s);
-        int a = in.nextInt();
-        System.out.println("You entered integer "+a);
-        float b = in.nextFloat();
-        System.out.println("You entered float "+b);
-        //TODO: You should always comment your code.
+        System.out.println("Your Name is " + s);
     }
 }
