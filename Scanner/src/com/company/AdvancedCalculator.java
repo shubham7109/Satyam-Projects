d1996e8fdc60f94d27f579afcdefcee291238e12
Satyam Sharma <satyamsharma251104@gmail.com>
2020-03-19 15:09:01 +0400

    Advanced Calculator
    Im stuck.. when i enter the value 1,2,3,4.. it stops when its supposed to the if statement.

---
+++ Scanner/src/com/company/AdvancedCalculator.java
@@ -0,0 +1,51 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class AdvancedCalculator {
+    public static void main(String[] args) {
+        System.out.println("If you would like to: \n add enter 1 \n subtract enter 2 \n multiply enter 3 \n divide enter 4  ");
+        Scanner in = new Scanner(System.in);
+        String d = in.nextLine();
+        if (d == "1") {
+            System.out.println("Insert a number you would like to add");
+            Scanner ir = new Scanner(System.in);
+            int a = Integer.parseInt(in.nextLine());
+            System.out.println("Insert another number");
+            Scanner ib = new Scanner(System.in);
+            int b = Integer.parseInt(ib.nextLine());
+            int c = a + b ;
+            System.out.println(" The required number is " + c);
+        } else if (d == "2") {
+            System.out.println("Insert a number you would like to subtract");
+            Scanner ir = new Scanner(System.in);
+            int a = Integer.parseInt(in.nextLine());
+            System.out.println("Insert another number");
+            Scanner ib = new Scanner(System.in);
+            int b = Integer.parseInt(ib.nextLine());
+            int c = a - b ;
+            System.out.println(" The required number is " + c);
+        } else if ( d == "3") {
+            System.out.println("Insert a number you would like to multiply");
+            Scanner ir = new Scanner(System.in);
+            int a = Integer.parseInt(in.nextLine());
+            System.out.println("Insert another number");
+            Scanner ib = new Scanner(System.in);
+            int b = Integer.parseInt(ib.nextLine());
+            int c = a * b ;
+            System.out.println(" The required number is " + c);
+        } else if ( d == "4") {
+            System.out.println("Insert a number you would like to add");
+            Scanner ir = new Scanner(System.in);
+            int a = Integer.parseInt(in.nextLine());
+            System.out.println("Insert another number you would like to add");
+            Scanner ib = new Scanner(System.in);
+            int b = Integer.parseInt(ib.nextLine());
+            int c = a / b ;
+            System.out.println(" The required number is " + c);
+
+        }
+
+    }
+
+    }
