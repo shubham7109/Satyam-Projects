b298907385b4f8496ee2b8454c393211c8480cde
Shubham Sharma <shubham@iastate.edu>
2020-03-20 00:09:07 -0500

    Fixed calculator

--- Scanner/src/com/company/AdvancedCalculator.java
+++ Scanner/src/com/company/AdvancedCalculator.java
@@ -7,7 +7,7 @@ public class AdvancedCalculator {
         System.out.println("If you would like to: \n add enter 1 \n subtract enter 2 \n multiply enter 3 \n divide enter 4  ");
         Scanner in = new Scanner(System.in);
         String d = in.nextLine();
-        if (d == "1") {
+        if (d.equals("1")) {
             System.out.println("Insert a number you would like to add");
             Scanner ir = new Scanner(System.in);
             int a = Integer.parseInt(in.nextLine());
@@ -16,7 +16,7 @@ public class AdvancedCalculator {
             int b = Integer.parseInt(ib.nextLine());
             int c = a + b ;
             System.out.println(" The required number is " + c);
-        } else if (d == "2") {
+        } else if (d.equals("2")) {
             System.out.println("Insert a number you would like to subtract");
             Scanner ir = new Scanner(System.in);
             int a = Integer.parseInt(in.nextLine());
@@ -25,7 +25,7 @@ public class AdvancedCalculator {
             int b = Integer.parseInt(ib.nextLine());
             int c = a - b ;
             System.out.println(" The required number is " + c);
-        } else if ( d == "3") {
+        } else if (d.equals("3")) {
             System.out.println("Insert a number you would like to multiply");
             Scanner ir = new Scanner(System.in);
             int a = Integer.parseInt(in.nextLine());
@@ -34,7 +34,7 @@ public class AdvancedCalculator {
             int b = Integer.parseInt(ib.nextLine());
             int c = a * b ;
             System.out.println(" The required number is " + c);
-        } else if ( d == "4") {
+        } else if (d.equals("4")) {
             System.out.println("Insert a number you would like to add");
             Scanner ir = new Scanner(System.in);
             int a = Integer.parseInt(in.nextLine());
