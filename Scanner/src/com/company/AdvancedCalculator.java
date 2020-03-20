3cf9a8ea6ccafd774313ac9fe19aea5cf6df087b
Shubham Sharma <shubham@iastate.edu>
2020-03-20 00:14:15 -0500

    Suggestion

--- Scanner/src/com/company/AdvancedCalculator.java
+++ Scanner/src/com/company/AdvancedCalculator.java
@@ -7,43 +7,52 @@ public class AdvancedCalculator {
         System.out.println("If you would like to: \n add enter 1 \n subtract enter 2 \n multiply enter 3 \n divide enter 4  ");
         Scanner in = new Scanner(System.in);
         String d = in.nextLine();
-        if (d.equals("1")) {
-            System.out.println("Insert a number you would like to add");
-            Scanner ir = new Scanner(System.in);
-            int a = Integer.parseInt(in.nextLine());
-            System.out.println("Insert another number");
-            Scanner ib = new Scanner(System.in);
-            int b = Integer.parseInt(ib.nextLine());
-            int c = a + b ;
-            System.out.println(" The required number is " + c);
-        } else if (d.equals("2")) {
-            System.out.println("Insert a number you would like to subtract");
-            Scanner ir = new Scanner(System.in);
-            int a = Integer.parseInt(in.nextLine());
-            System.out.println("Insert another number");
-            Scanner ib = new Scanner(System.in);
-            int b = Integer.parseInt(ib.nextLine());
-            int c = a - b ;
-            System.out.println(" The required number is " + c);
-        } else if (d.equals("3")) {
-            System.out.println("Insert a number you would like to multiply");
-            Scanner ir = new Scanner(System.in);
-            int a = Integer.parseInt(in.nextLine());
-            System.out.println("Insert another number");
-            Scanner ib = new Scanner(System.in);
-            int b = Integer.parseInt(ib.nextLine());
-            int c = a * b ;
-            System.out.println(" The required number is " + c);
-        } else if (d.equals("4")) {
-            System.out.println("Insert a number you would like to add");
-            Scanner ir = new Scanner(System.in);
-            int a = Integer.parseInt(in.nextLine());
-            System.out.println("Insert another number you would like to add");
-            Scanner ib = new Scanner(System.in);
-            int b = Integer.parseInt(ib.nextLine());
-            int c = a / b ;
-            System.out.println(" The required number is " + c);
+        switch (d) {
+            case "1": {
+                System.out.println("Insert a number you would like to add");
+                Scanner ir = new Scanner(System.in);
+                int a = Integer.parseInt(in.nextLine());
+                System.out.println("Insert another number");
+                Scanner ib = new Scanner(System.in);
+                int b = Integer.parseInt(ib.nextLine());
+                int c = a + b;
+                System.out.println(" The required number is " + c);
+                break;
+            }
+            case "2": {
+                System.out.println("Insert a number you would like to subtract");
+                Scanner ir = new Scanner(System.in);
+                int a = Integer.parseInt(in.nextLine());
+                System.out.println("Insert another number");
+                Scanner ib = new Scanner(System.in);
+                int b = Integer.parseInt(ib.nextLine());
+                int c = a - b;
+                System.out.println(" The required number is " + c);
+                break;
+            }
+            case "3": {
+                System.out.println("Insert a number you would like to multiply");
+                Scanner ir = new Scanner(System.in);
+                int a = Integer.parseInt(in.nextLine());
+                System.out.println("Insert another number");
+                Scanner ib = new Scanner(System.in);
+                int b = Integer.parseInt(ib.nextLine());
+                int c = a * b;
+                System.out.println(" The required number is " + c);
+                break;
+            }
+            case "4": {
+                System.out.println("Insert a number you would like to add");
+                Scanner ir = new Scanner(System.in);
+                int a = Integer.parseInt(in.nextLine());
+                System.out.println("Insert another number you would like to add");
+                Scanner ib = new Scanner(System.in);
+                int b = Integer.parseInt(ib.nextLine());
+                int c = a / b;
+                System.out.println(" The required number is " + c);
 
+                break;
+            }
         }
 
     }
