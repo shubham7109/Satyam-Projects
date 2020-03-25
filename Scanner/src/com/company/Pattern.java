dc36161e761ce37ab4a2d17ea24e0618179561b8
Shubham Sharma <shubham@iastate.edu>
2020-03-25 03:27:45 -0500

    Added new assignment

---
+++ Scanner/src/com/company/Pattern.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Pattern {
+
+    public static void main(String[] args) {
+
+
+        //TODO: Print this output using loops
+        /* Output:
+
+        ******
+        *****
+        ****
+        ***
+        **
+        *
+
+        */
+        System.out.println("Hello world");
+    }
+}
