dea62465f3f5f260b98eeb6d1bccad14a2bfc124
Shubham Sharma <shubham@iastate.edu>
2020-03-26 01:32:36 -0500

    New easy task

--- Scanner/src/com/company/Pattern.java
+++ Scanner/src/com/company/Pattern.java
@@ -8,12 +8,12 @@ public class Pattern {
         //TODO: Print this output using loops
         /* Output:
 
-        ******
-        *****
-        ****
-        ***
-        **
-        *
+        Enter numbers to print: 5
+        1 2 3 4 5
+        Enter numbers to print: 4
+        1 2 3 4
+        Enter numbers to print: 10
+        1 2 3 4 5 6 7 8 9 10
 
         */
         System.out.println("Hello world");
