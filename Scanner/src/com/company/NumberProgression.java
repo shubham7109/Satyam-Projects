548cfb9bdfa7ef0944722872d69fc0268b2ff025
Shubham Sharma <shubham@iastate.edu>
2020-03-27 18:06:09 -0500

    NEW ASSIGNMENT

--- Scanner/src/com/company/NumberProgression.java
+++ Scanner/src/com/company/NumberProgression.java
@@ -7,9 +7,18 @@ public class NumberProgression {
        System.out.println("How many digits would you want to print");
         Scanner in = new Scanner(System.in);
         int s = in.nextInt();
+        /*
         for (int q = 1; q <= s; q = q + 1 ) {
             System.out.print(q + " ");
         }
+        */
+
+        int count = 0;
+        while(count <= s){
+            System.out.print(count + " ");
+            count++; //This adds +1 to the value of count
+        }
+
 
     }
 
