799d3ccb56c2bfa9443858bd9f0663b0aae1c550
Shubham Sharma <shubham@iastate.edu>
2020-03-27 18:04:57 -0500

    Renamed file

---
+++ Scanner/src/com/company/OrderFood.java
@@ -0,0 +1,79 @@
+package com.company;
+
+public class OrderFood {
+
+    public static void main(String[] args) {
+
+
+        /*
+        TODO:
+        Write a program that will order you food and then perform a transaction
+
+        1. User inputs balance (amount of money they have)
+        2. User chooses items to add to cart (This should a in a while loop)
+        3. Once user is done adding items to cart program should add the total
+        4. Present the total amount to the user and then ask if the user would like to purchase
+        5. If user says yes: then subtract total from the balance
+        6. If balance < total then print "Insufficient Funds"
+        7. If balance > total then print the new updated balance and "Successful purchase"
+
+        Example:
+
+        Please input total balance: 14
+
+        Choose which items to add to cart:
+        1. Burrito: $5
+        2. Taco: $3
+        3. Sandwich: $10
+        4. Pasta: $6
+        5. Coke: $1
+        6. Checkout
+        Select which item to add to cart: 4
+
+        Added pasta to cart. Cart total is $6
+
+        Choose which items to add to cart:
+        1. Burrito: $5
+        2. Taco: $3
+        3. Sandwich: $10
+        4. Pasta: $6
+        5. Coke: $1
+        6. Checkout
+        Select which item to add to cart: 5
+
+        Added coke to cart. Cart total is $7
+
+        Choose which items to add to cart:
+        1. Burrito: $5
+        2. Taco: $3
+        3. Sandwich: $10
+        4. Pasta: $6
+        5. Coke: $1
+        6. Checkout
+        Select which item to add to cart: 2
+
+        Added taco to cart. Cart total is $10
+
+                Choose which items to add to cart:
+        1. Burrito: $5
+        2. Taco: $3
+        3. Sandwich: $10
+        4. Pasta: $6
+        5. Coke: $1
+        6. Checkout
+        Select which item to add to cart: 6
+
+        Checking out..
+
+        Cart Total: $10
+        User Balance: $14
+
+        Successful purchase
+        New user balance: $4
+
+
+        Exit program here.
+        */
+        System.out.println("Hello world");
+    }
+}
