2b9ae0281f19ccdb2ef0cbc198fb8ebd62d79a8b
Satyam Sharma <satyamsharma251104@gmail.com>
2020-04-02 09:31:52 +0400

    foodcal
    whats wrong with my code?

--- Scanner/src/com/company/OderFood.java
+++ Scanner/src/com/company/OderFood.java
@@ -4,11 +4,15 @@ import java.util.Scanner;
 
 public class OderFood {
     public static void main(String[] args) {
+
         Scanner in = new Scanner(System.in);
         System.out.println("Please input total balance");
+
         float balance = in.nextFloat();
         float checkoutbalance = 0;
+        int item = in.nextInt();
 
+        do {                                                                    //what is wrong with my loop why does it not display
         System.out.println("Choose which items to add to cart");
         System.out.println("1. Burrito: $5");
         System.out.println("2. Taco: $3");
@@ -17,18 +21,29 @@ public class OderFood {
         System.out.println("5. Coke: $1");
         System.out.println("6. Checkout");
         System.out.println("Select which item to add to cart: ");
-        int item = in.nextInt();
+        } while (item != 6);
+
         switch (item) {
-            case 1: checkoutbalance = 5;
-            case 2: checkoutbalance = 3;
-            case 3: checkoutbalance = 10;
-            case 4: checkoutbalance = 6;
-            case 5: checkoutbalance = 1;
-            case 6: float finalbalance = balance - checkoutbalance;
-                    System.out.println(checkoutbalance);
-
-
-        }
+                case 1:
+                    checkoutbalance = 5;
+                    break;
+                case 2:
+                    checkoutbalance = 3;
+                    break;
+                case 3:
+                    checkoutbalance = 10;
+                    break;
+                case 4:
+                    checkoutbalance = 6;
+                    break;
+                case 5:
+                    checkoutbalance = 1;
+                    break;
+                case 6:
+                    float finalbalance = balance - checkoutbalance;
+                    System.out.println(finalbalance);
+                    break;
+            }
     }
 }
 
