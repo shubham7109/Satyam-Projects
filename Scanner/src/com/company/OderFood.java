9bcde9983408741da827a255f015f0b598e34cb9
Satyam Sharma <satyamsharma251104@gmail.com>
2020-04-06 21:41:55 +0400

    update

--- Scanner/src/com/company/OderFood.java
+++ Scanner/src/com/company/OderFood.java
@@ -10,9 +10,9 @@ public class OderFood {
 
         float balance = in.nextFloat();
         float checkoutbalance = 0;
-        int item = in.nextInt();
+        int item;
 
-        do {                                                                    //what is wrong with my loop why does it not display
+        do {
         System.out.println("Choose which items to add to cart");
         System.out.println("1. Burrito: $5");
         System.out.println("2. Taco: $3");
@@ -21,23 +21,24 @@ public class OderFood {
         System.out.println("5. Coke: $1");
         System.out.println("6. Checkout");
         System.out.println("Select which item to add to cart: ");
+        item = in.nextInt();
         } while (item != 6);
 
         switch (item) {
                 case 1:
-                    checkoutbalance = 5;
+                    checkoutbalance += 5;
                     break;
                 case 2:
-                    checkoutbalance = 3;
+                    checkoutbalance += 3;
                     break;
                 case 3:
-                    checkoutbalance = 10;
+                    checkoutbalance += 10;
                     break;
                 case 4:
-                    checkoutbalance = 6;
+                    checkoutbalance += 6;
                     break;
                 case 5:
-                    checkoutbalance = 1;
+                    checkoutbalance += 1;
                     break;
                 case 6:
                     float finalbalance = balance - checkoutbalance;
