fae1725474acaf977eb10349c5cc5822d6b3321d
Satyam Sharma <satyamsharma251104@gmail.com>
2020-04-07 15:03:44 +0400

    FINISHED

--- Scanner/src/com/company/OderFood.java
+++ Scanner/src/com/company/OderFood.java
@@ -22,29 +22,42 @@ public class OderFood {
         System.out.println("6. Checkout");
         System.out.println("Select which item to add to cart: ");
         item = in.nextInt();
-        } while (item != 6);
-
         switch (item) {
-                case 1:
-                    checkoutbalance += 5;
-                    break;
-                case 2:
-                    checkoutbalance += 3;
-                    break;
-                case 3:
-                    checkoutbalance += 10;
-                    break;
-                case 4:
-                    checkoutbalance += 6;
-                    break;
-                case 5:
-                    checkoutbalance += 1;
-                    break;
-                case 6:
-                    float finalbalance = balance - checkoutbalance;
-                    System.out.println(finalbalance);
-                    break;
-            }
+            case 1:
+                checkoutbalance += 5;
+                System.out.println("Burrito has been added to your cart, Cart total is " + checkoutbalance + "Dhs");
+                break;
+            case 2:
+                checkoutbalance += 3;
+                System.out.println("Taco has been added to your cart, Cart total is " + checkoutbalance + "Dhs");
+                break;
+            case 3:
+                checkoutbalance += 10;
+                System.out.println("Sandwich has been added to your cart, Cart total is " + checkoutbalance + "Dhs");
+                break;
+            case 4:
+                checkoutbalance += 6;
+                System.out.println("Pasta has been added to your cart, Cart total is " + checkoutbalance + "Dhs");
+                break;
+            case 5:
+                checkoutbalance += 1;
+                System.out.println("Coke has been added to your cart, Cart total is " + checkoutbalance + "Dhs");
+                break;
+            case 6:
+                float finalbalance;
+                if (checkoutbalance <= balance) {
+                    finalbalance = balance - checkoutbalance;
+                    System.out.println("Total cart ammount is " + checkoutbalance + "Dhs \nTotal Balance entered is " + balance + "Dhs \nTo proceed with transaction enter 7 \nTo cancel transaction enter 8 ");
+                    float conformation = in.nextInt();
+                    if (conformation == 7){
+                        System.out.println("Transaction completed, Remaining balance is " + finalbalance + "Dhs");
+                    } else { System.out.println("Transaction Cancelled !");}
+                } else {
+                    System.out.println("Insufficient Funds !");
+                }
+                break;
+        }
+        }while (item != 6);
     }
 }
 
