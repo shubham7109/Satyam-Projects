465b811e0945f6b10e64b87087b3447e6168b01e
Shubham Sharma <shubham@iastate.edu>
2020-04-11 00:00:31 -0500

    Now its here

--- Scanner/src/com/company/Function.java
+++ Scanner/src/com/company/Function.java
@@ -1,4 +1,31 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Function {
+
+
+    public static void main(String[] args) {
+
+        // Learn aboutJava Functions! https://www.w3schools.com/java/java_methods.asp
+
+        //TODO
+        // Write a code prints "Enter your name:" using a printQuestion() function
+        // Enter your name using scanner using a getName() function. getName() function should return a String
+        // Store the returned name in a String variable
+        // Print out: "Hello, NAME" using the variable in the main() function.
+
+
+        //Here is some starter code for the functions you will use below. Good luck!
+
+    }
+
+    void printQuestion(){
+        //TODO, fill this in
+    }
+
+    void getName(){
+        //TODO, fill this in
+    }
+
 }
