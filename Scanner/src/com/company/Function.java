aa09ccb2a1ad258df3f275dbe4e0dd5f0ba77140
Satyam Sharma <satyamsharma251104@gmail.com>
2020-04-25 19:17:19 +0400

    Function
    How do i fix this?

--- Scanner/src/com/company/Function.java
+++ Scanner/src/com/company/Function.java
@@ -9,24 +9,31 @@ public class Function {
 
         // Learn aboutJava Functions! https://www.w3schools.com/java/java_methods.asp
 
+
         //TODO
         // Write a code prints "Enter your name:" using a printQuestion() function
         // Enter your name using scanner using a getName() function. getName() function should return a String
         // Store the returned name in a String variable
         // Print out: "Hello, NAME" using the variable in the helloMessage() function.
 
-        
+        printQuestion();
+        getName();
+        helloMessage();
+
 
     }
 
     private static void helloMessage() {
         //TODO
         // Print the hello message by retrieving the name as a parameter/argument
+        System.out.println("Hello, " + a);
     }
 
     static void printQuestion(){
         //TODO
         // Print the question "Enter you name" that is it :)
+        System.out.println("Enter your name");
+
 
     }
 
@@ -34,6 +41,8 @@ public class Function {
         //TODO
         // This function should not print anything
         // This function should get the name using scanner and then return the String back to main.
+        Scanner in = new Scanner(System.in);
+        String a = in.next();
     }
 
 }
