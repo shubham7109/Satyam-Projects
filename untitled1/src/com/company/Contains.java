9080be2866124031b12a6e052c6f964c12850921
Shubham Sharma <shubham71098@gmail.com>
2020-07-16 09:36:36 -0500

    Contrains problem added

---
+++ untitled1/src/com/company/Contains.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Contains {
+
+    public static void main(String[] args) {
+        //Don't worry if this looks confusing
+        ArrayList<Integer> list1 = new ArrayList<>(Arrays.asList(1, 2, 3, 3, 4, 4, 5)); //This list has some duplicates to remove
+        ArrayList<Integer> list2 = new ArrayList<>(Arrays.asList(1,2,3,4,5));           //This has no duplicates, so it shouldn't remove anything
+        ArrayList<Integer> list3 = new ArrayList<>(Arrays.asList(1,1,1,1,1,1,1,1));     //This should remove all except 1 item.
+
+        //Output
+        System.out.println(removeDuplicates(list1)); //Output: 1,2,3,4,5
+        System.out.println(removeDuplicates(list2)); //Output: 1,2,3,4,5
+        System.out.println(removeDuplicates(list3)); //Output: 1
+
+    }
+
+    private static ArrayList<Integer> removeDuplicates(ArrayList<Integer> numbers){
+        ArrayList<Integer> noDuplicates = new ArrayList<>();
+        //TODO Fill this method
+        // Hint: use ArrayList.contains() somewhere
+        return noDuplicates;
+    }
+
+}
