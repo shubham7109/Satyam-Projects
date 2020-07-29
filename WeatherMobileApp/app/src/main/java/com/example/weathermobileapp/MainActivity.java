e395c712e353e2489ae45cbc7f4c4d3d0012337d
Satyam Sharma <satyamsharma251104@gmail.com>
2020-07-29 06:31:24 +0400

    App

--- WeatherMobileApp/app/src/main/java/com/example/weathermobileapp/MainActivity.java
+++ WeatherMobileApp/app/src/main/java/com/example/weathermobileapp/MainActivity.java
@@ -12,10 +12,10 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.squareup.picasso.Picasso;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
@@ -27,28 +27,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        callAPI("Dubai",API_KEY);
+        callAPI();
     }
 
     /**
      * Makes the API get request. Nothing needs to be edited here.
      * More info: https://developer.android.com/training/volley
-     * @param location Location of the user
-     * @param api_key Key of the API call.
      */
-    private void callAPI(String location, String api_key) {
+    private void callAPI() {
         // Instantiate the RequestQueue.
         RequestQueue queue = Volley.newRequestQueue(this);
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, "https://api.openweathermap.org/data/2.5/weather?q="+location+"&appid="+api_key, null, new Response.Listener<JSONObject>() {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, "https://api.openweathermap.org/data/2.5/weather?q=Dubai&appid="+ MainActivity.API_KEY, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
-                setUpViews(response);
+                try {
+                    setUpViews(response);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
                 Toast.makeText(MainActivity.this, "Error: " + error.getMessage(), Toast.LENGTH_SHORT).show();
-
             }
         });
         // Add the request to the RequestQueue.
@@ -61,9 +62,38 @@ public class MainActivity extends AppCompatActivity {
      * @param response Response from the API call
      * @return WeatherModel object with the weather values set.
      */
-    private WeatherModel getWeatherModel(JSONObject response) {
+    private WeatherModel getWeatherModel(JSONObject response) throws JSONException {
         //TODO
-        return new WeatherModel();
+        JSONObject responseObject = new JSONObject(String.valueOf(response));
+        String locationName = responseObject.getString("name");
+        String iconID = responseObject.getJSONArray("weather").getJSONObject(0).getString("id");
+        int temperature = (int) ((responseObject.getJSONObject("main").getInt("temp")) - 273.15);
+        String weatherDescription = responseObject.getJSONArray("weather").getJSONObject(0).getString("description");
+        int feelsLike = (int) ((responseObject.getJSONObject("main").getDouble("feels_like")) - 273.25);
+        String windDescription = "The wind speed and direction in degree is " + Math.round((responseObject.getJSONObject("wind").getDouble("speed"))*3.6) + " (Km/h) from the " + directionConverter(responseObject.getJSONObject("wind").getInt("deg"));
+        return new WeatherModel(locationName, iconID, temperature, feelsLike, weatherDescription, windDescription);
+    }
+
+    public String directionConverter(int directionDegree) {
+        String direction = null;
+        if (directionDegree < 30 || directionDegree > 330) {
+            direction = "North";
+        } else if (directionDegree > 30 && directionDegree < 60) {
+            direction = "North East";
+        } else if (directionDegree > 60 && directionDegree < 120) {
+            direction = "East";
+        } else if (directionDegree > 120 && directionDegree < 150) {
+            direction = "South East";
+        } else if (directionDegree > 150 && directionDegree < 210) {
+            direction = "South";
+        } else if (directionDegree > 210 && directionDegree < 240) {
+            direction = "South West";
+        } else if (directionDegree > 240 && directionDegree < 300) {
+            direction = "West";
+        } else if (directionDegree > 300 && directionDegree < 330) {
+            direction = "North West";
+        }
+        return direction;
     }
 
     //TODO: Step 2
@@ -71,13 +101,22 @@ public class MainActivity extends AppCompatActivity {
      * Set up the TextViews using the weatherModel object.
      * @param response JSON response from API call.
      */
-    private void setUpViews(JSONObject response) {
+    private void setUpViews(JSONObject response) throws JSONException {
         WeatherModel weatherModel = getWeatherModel(response);
         //TODO: Use weatherModel to set up the TextViews...
         //Example 1:
-        //TextView location = findViewById(R.id.location);
-        //location.setText(weatherModel.getLocationName()); //Should be "Dubai"
+        TextView location = findViewById(R.id.location);
+        location.setText(weatherModel.getLocationName()); //Should be "Dubai"
         //Now do the rest ...
+        setIcon(weatherModel.getIconID());
+        TextView temperature = findViewById(R.id.temperature);
+        temperature.setText(weatherModel.getTemperature());
+        TextView feelsLike = findViewById(R.id.feels_like);
+        feelsLike.setText(weatherModel.getFeelsLike());
+        TextView weatherDescription = findViewById(R.id.description);
+        weatherDescription.setText(weatherModel.getWeatherDescription());
+        TextView windDescription = findViewById(R.id.wind);
+        windDescription.setText(weatherModel.getWindDescription());
     }
 
     /**
--- WeatherMobileApp/app/src/main/java/com/example/weathermobileapp/WeatherModel.java
+++ WeatherMobileApp/app/src/main/java/com/example/weathermobileapp/WeatherModel.java
@@ -4,21 +4,18 @@ public class WeatherModel {
 
     private String locationName;
     private String iconID;
-    private String temperature;
-    private String feelsLike;
+    private int temperature;
+    private int feelsLike;
     private String weatherDescription;
     private String windDescription;
 
-    public WeatherModel(String locationName, String iconID, String temperature, String feelsLike, String weatherDescription, String windDescription) {
+    public WeatherModel(String locationName, String iconID, int temperature, int feelsLike, String weatherDescription, String windDescription) {
         this.locationName = locationName;
         this.iconID = iconID;
         this.temperature = temperature;
         this.feelsLike = feelsLike;
         this.weatherDescription = weatherDescription;
         this.windDescription = windDescription;
-    }
-
-    public WeatherModel() {
     }
 
     public String getLocationName() {
@@ -29,11 +26,11 @@ public class WeatherModel {
         return iconID;
     }
 
-    public String getTemperature() {
+    public int getTemperature() {
         return temperature;
     }
 
-    public String getFeelsLike() {
+    public int getFeelsLike() {
         return feelsLike;
     }
 
